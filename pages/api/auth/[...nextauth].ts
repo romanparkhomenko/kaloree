77fb6509d5eb5dc86d7689f070a2519a8890dde8
romanparkhomenko <rsparkhomenko@gmail.com>
2021-01-01 20:51:22 -0700

    Google SSO

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,18 @@
-import { NextApiHandler } from "next";
-import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
-import Adapters from "next-auth/adapters";
-import prisma from '../../../lib/prisma'
-
+import { NextApiHandler } from 'next';
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import Adapters from 'next-auth/adapters';
+import prisma from '../../../lib/prisma';
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
 
 const options = {
   providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Providers.Email({
       server: {
         host: process.env.SMTP_HOST,
@@ -27,5 +30,5 @@ const options = {
   pages: {
     signIn: '/',
     signOut: '/login',
-  }
+  },
 };
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import {signIn, useSession} from "next-auth/client";
+import React, { useState } from 'react';
+import { signIn, useSession } from 'next-auth/client';
 
 // reactstrap components
 import {
@@ -15,13 +15,13 @@ import {
   InputGroup,
   Row,
   Col,
-} from "reactstrap";
+} from 'reactstrap';
 // layout for this page
-import Auth from "../layouts/Auth";
-import {useRouter} from "next/router";
+import Auth from '../layouts/Auth';
+import { useRouter } from 'next/router';
 
 const Login: React.FC = () => {
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState('');
 
   const router = useRouter();
   const [session] = useSession();
@@ -31,41 +31,59 @@ const Login: React.FC = () => {
   }
 
   return (
-      <Auth>
-        <Col lg="5" md="7">
-          <Card className="bg-secondary shadow border-0">
-            <CardBody className="px-lg-5 py-lg-5">
-              <div className="text-center text-muted mb-4">
-                <small>Sign in with your email, we'll send you a link.</small>
-              </div>
-              <Form role="form">
-                <FormGroup className="mb-3">
-                  <InputGroup className="input-group-alternative">
-                    <InputGroupAddon addonType="prepend">
-                      <InputGroupText>
-                        <i className="ni ni-email-83" />
-                      </InputGroupText>
-                    </InputGroupAddon>
-                    <Input
-                        placeholder="Email"
-                        type="email"
-                        id="email"
-                        autoComplete="new-email"
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                  </InputGroup>
-                </FormGroup>
-                <div className="text-center">
-                  <Button className="w-100" color="primary" type="button" onClick={() => signIn('email', {email: email})}>
-                    Sign in
-                  </Button>
-                </div>
-              </Form>
-            </CardBody>
-          </Card>
-        </Col>
-      </Auth>
+    <Auth>
+      <Col lg="5" md="7">
+        <Card className="bg-secondary shadow border-0">
+          <CardBody className="px-lg-5 py-lg-5">
+            <div className="text-center text-muted mb-4">
+              <small>Sign in with Google</small>
+            </div>
+            <div className="btn-wrapper text-center">
+              <Button
+                className="btn-neutral btn-icon w-100"
+                color="default"
+                type="button"
+                onClick={() => signIn('google')}
+              >
+                <span className="btn-inner--icon">
+                  <img alt="..." src={require('assets/img/icons/common/google.svg')} />
+                </span>
+                <span className="btn-inner--text">Google</span>
+              </Button>
+            </div>
+            {/*<Form role="form">*/}
+            {/*  <FormGroup className="mb-3">*/}
+            {/*    <InputGroup className="input-group-alternative">*/}
+            {/*      <InputGroupAddon addonType="prepend">*/}
+            {/*        <InputGroupText>*/}
+            {/*          <i className="ni ni-email-83" />*/}
+            {/*        </InputGroupText>*/}
+            {/*      </InputGroupAddon>*/}
+            {/*      <Input*/}
+            {/*        placeholder="Email"*/}
+            {/*        type="email"*/}
+            {/*        id="email"*/}
+            {/*        autoComplete="new-email"*/}
+            {/*        onChange={e => setEmail(e.target.value)}*/}
+            {/*      />*/}
+            {/*    </InputGroup>*/}
+            {/*  </FormGroup>*/}
+            {/*  <div className="text-center">*/}
+            {/*    <Button*/}
+            {/*      className="w-100"*/}
+            {/*      color="primary"*/}
+            {/*      type="button"*/}
+            {/*      onClick={() => signIn('email', { email: email })}*/}
+            {/*    >*/}
+            {/*      Sign in*/}
+            {/*    </Button>*/}
+            {/*  </div>*/}
+            {/*</Form>*/}
+          </CardBody>
+        </Card>
+      </Col>
+    </Auth>
   );
-}
+};
 
 export default Login;
